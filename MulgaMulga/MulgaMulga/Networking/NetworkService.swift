a57ce9393f173c8cc3366e214d5007037026cd2b
JiSoo Kim <ghis22130@naver.com>
2021-09-16 13:27:14 +0900

    feat: 그룹 분류 및 NetworkService 추상화

---
+++ MulgaMulga/MulgaMulga/Networking/NetworkService.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkService.swift
+//  MulgaMulga
+//
+//  Created by 지북 on 2021/09/16.
+//
+
+import Foundation
+
+protocol NetworkService { }
+
+protocol APIService: NetworkService {
+    
+}
